bb3b7a4c605389fcca2f68849c079c9272dd7746
irshadansari49 <89354997+irshadansari49@users.noreply.github.com>
2021-10-13 17:39:50 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class NaturalNumber {
+    public static void main(String[] args){
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("loop ends");
+    }
+}
